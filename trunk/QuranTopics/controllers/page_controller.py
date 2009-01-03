01ac0a49fd177b209f21e45d911f50655da8fc6a
gaussianera <gaussianera@c4ea9544-b9ab-11dd-b612-3927438af0ba>
2009-01-03 21:24:11 +0000

    implement user login/logout. Doesn't do anything with the info yet

--- trunk/QuranTopics/controllers/page_controller.py
+++ trunk/QuranTopics/controllers/page_controller.py
@@ -1,6 +1,7 @@
 import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class PageController(webapp.RequestHandler):
     
@@ -8,15 +9,28 @@ class PageController(webapp.RequestHandler):
     
     def get(self):
         self.template_values = {}
+        self.set_user()
         view = self.perform_get()
         self.display_view(view)
         
         
     def post(self):
         self.template_values = {}
+        self.set_user()
         view = self.perform_post()
         self.display_view(view)
         
+
+    def set_user(self):
+        user = users.get_current_user()
+        if user:
+            self.template_values['user_nickname'] = user.nickname()
+            user_link = users.create_logout_url('/')
+        else:
+            user_link = users.create_login_url(self.request.uri)
+
+        self.template_values['user_link'] = user_link
+            
     
     def display_view(self, view):
         path = self.get_view_path(view)
