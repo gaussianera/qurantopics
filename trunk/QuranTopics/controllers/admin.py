2a867de050bdd1adba80628e7a774f768fa49c28
gaussianera <gaussianera@c4ea9544-b9ab-11dd-b612-3927438af0ba>
2009-02-02 15:09:23 +0000

    added admin function to reput a sura's ayat, to fix the index issue

--- trunk/QuranTopics/controllers/admin.py
+++ trunk/QuranTopics/controllers/admin.py
@@ -4,7 +4,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-from controllers.entities import Sura
+from controllers.entities import Sura, Aya
 
 
 class RemoveSura(webapp.RequestHandler):
@@ -15,8 +15,21 @@ class RemoveSura(webapp.RequestHandler):
         db.delete(sura.aya_set)
 
 
+class ReputSura(webapp.RequestHandler):
+
+    def get(self):
+        sura_number = int(self.request.get('sura'))
+        sura = Sura.gql("WHERE number = :number ", number = sura_number).fetch(1)[0]
+        ayat = Aya.gql("where sura = :sura", sura = sura).fetch(1000)
+        for aya in ayat:
+            db.put(aya)
+        self.response.out.write ("reput num of ayat: " + str(len(ayat)))
+        
+
+
 application = webapp.WSGIApplication(
-                                     [('/admin/remove_sura', RemoveSura)],
+                                     [('/admin/remove_sura', RemoveSura),
+                                      ('/admin/reput_sura', ReputSura)],
                                      debug=True)
 
 def main():
