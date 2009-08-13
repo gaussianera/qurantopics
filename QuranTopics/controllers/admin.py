3a4d97b589efcfdf007d641a4a7877f7a52b233d
gaussianera <gaussianera@c4ea9544-b9ab-11dd-b612-3927438af0ba>
2009-08-13 22:57:10 +0000

    added code to display topics that reference the aya

--- QuranTopics/controllers/admin.py
+++ QuranTopics/controllers/admin.py
@@ -1,10 +1,12 @@
+# coding: utf8
+
 import logging
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-from controllers.entities import Sura, Aya
+from controllers.entities import Sura, Aya, Topic
 from controllers.page_controller import PageController
 
 
@@ -33,8 +35,23 @@ class EditAya(PageController):
     def perform_get(self):
         sura_number = int(self.request.get('sura'))
         aya_number = int(self.request.get('aya'))
-        aya = Aya.get_by_sura_and_aya_number(sura_number, aya_number)
-        self.template_values['aya'] = aya
+        
+        sura = Sura.get_by_number(sura_number)
+        ayat = sura.aya_set
+        ayat.filter('number =', aya_number)
+        ayat = ayat.fetch(1000)
+
+        aya_topics = []
+        for aya in ayat:
+            aya_topics.append("مفتاح = " + str(aya.key()));
+            topics = Topic.gql("WHERE ayat_keys = :aya_key ", aya_key = aya.key()).fetch(1000)
+            for topic in topics:
+                aya_topics.append("موضوع = " + str(topic.topic_id) + ":" + str(topic.title))
+        
+        
+        self.template_values['aya'] = ayat[0]
+        self.template_values['aya_topics'] = aya_topics
+        self.template_values['ayat'] = ayat
  
         return 'edit_aya.html'
         
