73e424947638634f2512bffacac932595a40146b
gaussianera <gaussianera@c4ea9544-b9ab-11dd-b612-3927438af0ba>
2009-08-14 23:43:04 +0000

    complete updating and deleting repeated ayas

--- QuranTopics/controllers/admin.py
+++ QuranTopics/controllers/admin.py
@@ -36,6 +36,11 @@ class EditAya(PageController):
         sura_number = int(self.request.get('sura'))
         aya_number = int(self.request.get('aya'))
         
+        return self.display_page(sura_number, aya_number)
+        
+        
+    def display_page(self, sura_number, aya_number):
+        
         sura = Sura.get_by_number(sura_number)
         ayat = sura.aya_set
         ayat.filter('number =', aya_number)
@@ -44,7 +49,7 @@ class EditAya(PageController):
         aya_topics = []
         for aya in ayat:
             aya_topics.append("مفتاح = " + str(aya.key()));
-            topics = Topic.gql("WHERE ayat_keys = :aya_key ", aya_key = aya.key()).fetch(1000)
+            topics = self.get_topics_for_aya(aya)
             for topic in topics:
                 aya_topics.append("موضوع = " + str(topic.topic_id) + ":" + str(topic.title))
         
@@ -52,9 +57,31 @@ class EditAya(PageController):
         self.template_values['aya'] = ayat[0]
         self.template_values['aya_topics'] = aya_topics
         self.template_values['ayat'] = ayat
+        self.template_values['show_delete'] = len(ayat) > 1
  
         return 'edit_aya.html'
+    
         
+    def perform_post(self):
+        if (self.request.get('edit')):
+            aya_key = self.request.get('aya_key')
+            aya = db.get(aya_key)
+            aya.content = self.request.get('aya_content')
+            db.put(aya)
+            return self.display_page(aya.sura.number, aya.number)
+        
+        elif (self.request.get('delete')):
+            aya_key = self.request.get('aya_key')
+            aya = db.get(aya_key)
+            topics = self.get_topics_for_aya(aya)
+            if len(topics) == 0:
+                db.delete(aya)
+            return self.display_page(aya.sura.number, aya.number)
+        
+        
+    def get_topics_for_aya(self, aya):
+        return Topic.gql("WHERE ayat_keys = :aya_key ", aya_key = aya.key()).fetch(1000)
+
 
 
 application = webapp.WSGIApplication(
